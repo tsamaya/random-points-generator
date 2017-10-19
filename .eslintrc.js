ba2760a7efabff6b59ef0d763a847a5ef29c482d
arnaud ferrand <arferrand@gmail.com>
2017-10-19 11:35:39 +0100

    single quote

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "env" : {
-      "mocha" : true,
-    }
+  extends: 'airbnb-base',
+  plugins: [
+    'import'
+  ],
+  env: {
+    'mocha': true,
+  }
 };
